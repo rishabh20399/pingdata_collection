41f80b6f35d10730b108e8e91ab8c2093e74ce68
rishabh20399 <88918267+rishabh20399@users.noreply.github.com>
2023-11-03 02:00:31 +0530

    Update test.sh

--- airtel/test.sh
+++ airtel/test.sh
@@ -6,12 +6,16 @@ script_path="/home/useland/pingdata_collection/airtel/DataCollection2.py"
 # Path to your Git repository
 repository_path="/home/useland/pingdata_collection"
 
+# Username and password (replace with your actual credentials)
+username="rishabh20399"
+password="GitHub@10p20vt1005"
+
 # Specify the current time as the starting time
 start_time=$(date +'%H:%M')
 
 # Loop for 4 iterations
 for ((iteration=0; iteration<4; iteration++)); do
-    execution_time=$(date +'%Y-%m-%d')" $start_time"
+    execution_time="$(date +'%Y-%m-%d') $start_time"
 
     # Wait for the previous iteration to complete (if not the first iteration)
     if [[ $iteration -gt 0 ]]; then
@@ -26,9 +30,19 @@ for ((iteration=0; iteration<4; iteration++)); do
     start_time=$(date -d "$start_time +1 minute" +'%H:%M')
 done
 
-# After 4 iterations, commit and push changes to Git
+# Wait for all iterations to complete
+while jobs %% &>/dev/null; do
+    sleep 600
+done
+
+# After all 4 iterations, commit and push changes to Git
 cd "$repository_path"
+
+# Set Git credentials (use a secure method, e.g., SSH keys or credential manager)
+# Replace the following line with a secure credential setup
+git config credential.helper store <<< "https://$username:$password@github.com"
 git add .
 git commit -m "Automated data collection"
 git push
+
 cd "/home/"  # Return to the home directory
