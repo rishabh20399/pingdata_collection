88b5fc538730fcda2d332020e095d3f2da3f0620
rishabh20399 <88918267+rishabh20399@users.noreply.github.com>
2023-11-03 16:41:51 +0530

    Update test.sh

--- airtel/test.sh
+++ airtel/test.sh
@@ -6,12 +6,8 @@ script_path="/home/useland/pingdata_collection/airtel/DataCollection2.py"
 # Path to your Git repository
 repository_path="/home/useland/pingdata_collection"
 
-# # Username and password (replace with your actual credentials)
-# username="your_username"
-# password="your_password"
-
-# Specify the starting time as "22:00"
-start_time="16:45"
+# Specify the starting time as "2200" for 22:00
+start_time="1645"  # Use your desired start time in HHMM format
 
 # Get the current date in 'YYYY-MM-DD' format
 current_date=$(date +'%Y-%m-%d')
@@ -37,14 +33,3 @@ while jobs %% &>/dev/null; do
     sleep 600
 done
 
-# # After all 4 iterations, commit and push changes to Git
-# cd "$repository_path"
-
-# # Set Git credentials (use a secure method, e.g., SSH keys or credential manager)
-# # Replace the following line with a secure credential setup
-# git config credential.helper store <<< "https://$username:$password@github.com"
-# git add .
-# git commit -m "Automated data collection"
-# git push
-
-# cd "/home/"  # Return to the home directory
