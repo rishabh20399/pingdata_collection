872890a1f3fa31734c14484060b4a02ea3178263
rishabh20399 <88918267+rishabh20399@users.noreply.github.com>
2023-11-03 16:23:13 +0530

    Update test.sh

--- airtel/test.sh
+++ airtel/test.sh
@@ -6,12 +6,12 @@ script_path="/home/useland/pingdata_collection/airtel/DataCollection2.py"
 # Path to your Git repository
 repository_path="/home/useland/pingdata_collection"
 
-# Username and password (replace with your actual credentials)
-username="rishabh20399"
-password="GitHub@10p20vt1005"
+# # Username and password (replace with your actual credentials)
+# username="your_username"
+# password="your_password"
 
-# Specify the current time as the starting time
-start_time=$(date +'%H:%M')
+# Specify the starting time as "22:00"
+start_time="22:00"
 
 # Loop for 4 iterations
 for ((iteration=0; iteration<4; iteration++)); do
@@ -27,7 +27,6 @@ for ((iteration=0; iteration<4; iteration++)); do
     # Schedule the script using 'at'
     echo "python3 $script_path" | at "$execution_time"
     echo "Scheduled python script to run at $execution_time"
-    start_time=$(date -d "$start_time +1 minute" +'%H:%M')
 done
 
 # Wait for all iterations to complete
@@ -35,14 +34,14 @@ while jobs %% &>/dev/null; do
     sleep 600
 done
 
-# After all 4 iterations, commit and push changes to Git
-cd "$repository_path"
+# # After all 4 iterations, commit and push changes to Git
+# cd "$repository_path"
 
-# Set Git credentials (use a secure method, e.g., SSH keys or credential manager)
-# Replace the following line with a secure credential setup
-git config credential.helper store <<< "https://$username:$password@github.com"
-git add .
-git commit -m "Automated data collection"
-git push
+# # Set Git credentials (use a secure method, e.g., SSH keys or credential manager)
+# # Replace the following line with a secure credential setup
+# git config credential.helper store <<< "https://$username:$password@github.com"
+# git add .
+# git commit -m "Automated data collection"
+# git push
 
-cd "/home/"  # Return to the home directory
+# cd "/home/"  # Return to the home directory
