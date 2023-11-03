7cc3ea9b535a497de71067d2b9bea1aa9a13fea3
rishabh20399 <88918267+rishabh20399@users.noreply.github.com>
2023-11-03 18:27:24 +0530

    pingUpdate

--- airtel/test.sh
+++ airtel/test.sh
@@ -9,7 +9,7 @@ mkdir /home/userland/pingdata_collection/airtel/data/
 repository_path="/home/userland/pingdata_collection"
 
 # Specify the starting time as "1645" for 16:45
-start_time="1800"  # Use your desired start time in HHMM format
+start_time="1835"  # Use your desired start time in HHMM format
 
 # Get the current date in 'YYYY-MM-DD' format
 current_date=$(date +'%Y-%m-%d')
@@ -32,13 +32,16 @@ for ((iteration=0; iteration<4; iteration++)); do
 
 done
 
+
+# Configure your Git identity
+git config --global user.email "rishabh20399@iiitd.ac.in"
+git config --global user.name "rishabh20399"
+git config --global init.defaultBranch main
+
 # After all 4 iterations, commit and push changes to Git
 cd '/home/userland/pingdata_collection/airtel/data/'
 git init
 git add .
 git commit -m "Add files from data"
 
-cd "$repository_path"
-# # Include your Git commands for add, commit, and push here
 
-# cd "/home/"  # Return to the home directory
