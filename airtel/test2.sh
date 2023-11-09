5ef2ed15c811b040429a46527e782805919a5499
rishabh20399 <88918267+rishabh20399@users.noreply.github.com>
2023-11-09 21:01:35 +0530

    Update test2.sh

--- airtel/test2.sh
+++ airtel/test2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Specify the starting time in HH:MM format (24-hour format)
-start_time="20:50"  # Set your desired start time here
+start_time="21:05"  # Set your desired start time here
 
 # Number of days to run the script
 total_days=1
@@ -34,16 +34,36 @@ for ((day=1; day<=$total_days; day++)); do
     fi
 
     # Wait until the next run time
+    echo "Waiting for the scheduled time..."
     sleep $wait_time
 
     # Print a message for each iteration
     echo "Day $day: Running at $(date +'%H:%M:%S')..."
 
     # Schedule the script to run in the background and detach it from the terminal
+    echo "Running ping data collection..."
     nohup python3 "$script1_path" >/dev/null 2>&1 &
 
     # Run script2 in the background
+    echo "Running traceroute data collection..."
     nohup python3 "$script2_path" >/dev/null 2>&1 &
+
+    # Configure your Git identity
+    git config --global user.email "rishabh20399@iiitd.ac.in"
+    git config --global user.name "rishabh20399"
+    # git config --global init.defaultBranch main
+
+    # After all 4 iterations, commit and push changes to Git
+    cd /data/data/com.termux/files/home/pingdata_collection
+    git remote set-url origin git@github.com:rishabh20399/pingdata_collection.git
+
+    git checkout -b my-changes
+    git add /data/data/com.termux/files/home/pingdata_collection/airtel/data
+    git commit -m "Auto commit at $(date +'%Y-%m-%d %H:%M:%S')"
+    git push origin my-changes
+
+
+    echo "Done for the day..."
 done
 
 # You can add code here to commit and push your changes to Git
