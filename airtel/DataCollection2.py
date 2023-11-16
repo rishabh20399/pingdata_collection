8d76cfda347b2ca5ac4d812f18a713d26b29ac83
rishabh20399 <88918267+rishabh20399@users.noreply.github.com>
2023-11-16 08:45:29 +0530

    I knew you were trouble...

--- airtel/DataCollection2.py
+++ airtel/DataCollection2.py
@@ -94,11 +94,11 @@ def collect_ping_data(num, domain, ping_count, ping_size):
     return data
 
 # Read websites from a text file
-with open('/data/data/com.termux/files/home/pingdata_collection/airtel/domain_list2.txt', 'r') as file:
+with open('/data/data/com.termux/files/home/pingdata_collection/airtel/final_domains.txt', 'r') as file:
     domain_names = file.read().splitlines()
 
 # Set the number of pings and ping size
-ping_count = 40
+ping_count = 20
 ping_size = 64
 num=0
 
